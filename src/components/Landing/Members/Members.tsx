553a14c4d837ca6bdb331cf7fc6cd2d9dec28f7e
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-06-23 22:03:53 -0300

    landing

--- src/components/Landing/Members/Members.tsx
+++ src/components/Landing/Members/Members.tsx
@@ -1,7 +1,11 @@
 import styles from './Members.module.css';
 
 const Members = () => {
-  return <div className={styles.members}>Members</div>;
+  return (
+    <div className={styles.members}>
+      <div className={styles.img_container}>IMG CONTAINER</div>
+    </div>
+  );
 };
 
 export default Members;
