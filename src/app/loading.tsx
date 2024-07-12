ab20e8d23529714e283ea7da72ad05587a8f1ca4
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-07-12 11:15:12 -0300

    configs

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import './globals.css';
+const loading = () => {
+  return (
+    <div className='loader-container'>
+      <h1 className='info-message'>Loading...</h1>
+    </div>
+  );
+};
+
+export default loading;
