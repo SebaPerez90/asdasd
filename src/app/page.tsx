bda7f8db47c238c6bfb12c67d0f8d607fab5c276
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-07-30 00:32:12 -0300

    landing mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Landing from '@/components/Home/Landing/Landing';
 import About from '@/components/Home/About/About';
 import Services from '@/components/Home/Services/Services';
+import Footer from '@/components/Footer/Footer';
 
 const Home = () => {
   return (
-    <main>
-      <Landing />
-      <About />
-      <Services />
-    </main>
+    <>
+      <main>
+        <Landing />
+        <About />
+        <Services />
+      </main>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.footer_container}>
+      <p>Footer</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+        repellat veritatis illo explicabo aliquid voluptas sit est similique,
+        dolore quod ullam distinctio, labore voluptates, quia nemo odit
+        inventore mollitia. Soluta.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
