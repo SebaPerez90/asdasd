2db06a87da06bf91f69f5ebe059d5fd7d6375392
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-01 11:34:58 -0300

    landing mock

--- src/components/Home/Services/OurServices/OurServices.tsx
+++ src/components/Home/Services/OurServices/OurServices.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image';
 import styles from './OurServices.module.css';
+import categories from '@/utils/categories-mock.json';
 
 const OurServices = () => {
-  return <div className={styles.our_services_container}>OurServices</div>;
+  return (
+    <div className={styles.our_services_container}>
+      <h1>Nuestros Servicios</h1>
+      <p>
+        Explora las categorías con mayor demanda. Puedes sugerir nuevas
+        categorías contactándonos.
+      </p>
+      <div className={styles.grid_container}>
+        {categories.map((category, index) => (
+          <div
+            className={styles.grid_item}
+            key={index}>
+            <div className={styles.img_container}>
+              <Image
+                width={500}
+                height={500}
+                src={category.image}
+                alt='categorie-img'
+              />
+            </div>
+            <h3>{category.categoria}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OurServices;
