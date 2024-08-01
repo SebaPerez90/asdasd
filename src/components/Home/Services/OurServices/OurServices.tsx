899ac4aac4db829c5aa5fb7f4001837422ccebbe
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-01 14:24:13 -0300

    landing mock

--- src/components/Home/Services/OurServices/OurServices.tsx
+++ src/components/Home/Services/OurServices/OurServices.tsx
@@ -24,6 +24,12 @@ const OurServices = () => {
               />
             </div>
             <h3>{category.categoria}</h3>
+            <ul>
+              <span>+</span>
+              {category.profesiones.map((profession, index) => (
+                <li key={index}>{profession}</li>
+              ))}
+            </ul>
           </div>
         ))}
       </div>
