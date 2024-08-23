0abd7a01d96edd59c80391b0fa62b8a37b18a41c
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-23 08:13:54 -0300

    authentication flow

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,16 +15,5 @@
 // const handler = NextAuth(authOptions);
 
 // export { handler as GET, handler as POST };
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-});
-
+import { handlers } from '../../../auth';
 export const { GET, POST } = handlers;
